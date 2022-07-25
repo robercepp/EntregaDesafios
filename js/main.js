22658f81285a6c2f60b7aa369d54fdc0fd400de9
Robertino Cepparo <robercepp@hotmail.com>
2022-07-25 15:09:47 -0300

    Se corrige bug en quitar items

--- js/main.js
+++ js/main.js
@@ -11,7 +11,8 @@ const iva = 1.21;
 
 //Constructor de Productos.
 class Producto {
-    constructor(tipo, nombre, precio, stockDisponible) {
+    constructor(id, tipo, nombre, precio, stockDisponible) {
+        this.id = id;
         this.tipo = tipo;
         this.nombre = nombre;
         this.precio = parseFloat(precio);
@@ -25,6 +26,7 @@ class Producto {
                 if (cantidad >= 1) {
                     this.stock = this.stock - cantidad;
                     carritoDeCompras.push({
+                        id: this.id,
                         tipo: this.tipo,
                         cantidad: cantidad,
                         nombre: this.nombre,
@@ -49,22 +51,22 @@ class Producto {
 
 // Creando Productos
 //Libretas
-const libretaChicasGamer = new Producto("Libreta", "Libreta de Chicas Gamer", 12.5, 10);
-const libretaSixFanarts = new Producto("Libreta", "Libreta de Six Fanarts", 10.5, 15);
-const libretaLuluMartins = new Producto("Libreta", "Libreta de Lulu Martins", 11.0, 6);
-const libretaChristineHug = new Producto("Libreta", "Libreta de Christine Hug", 12.0, 8);
+const libretaChicasGamer = new Producto(0, "Libreta", "Libreta de Chicas Gamer", 12.5, 10);
+const libretaSixFanarts = new Producto(1, "Libreta", "Libreta de Six Fanarts", 10.5, 15);
+const libretaLuluMartins = new Producto(2, "Libreta", "Libreta de Lulu Martins", 11.0, 6);
+const libretaChristineHug = new Producto(3, "Libreta", "Libreta de Christine Hug", 12.0, 8);
 
 //Stickers
-const stickerSirenas = new Producto("Sticker", "Stickers de Sirenas", 3.50, 6);
-const stickerChicas = new Producto("Sticker", "Stickers de Chicas", 3.0, 7);
-const stickerHalloween = new Producto("Sticker", "Stickers de Halloween", 2.75, 10);
-const stickerAnimales = new Producto("Sticker", "Stickers de Animales", 3.5, 10);
+const stickerSirenas = new Producto(4, "Sticker", "Stickers de Sirenas", 3.50, 6);
+const stickerChicas = new Producto(5, "Sticker", "Stickers de Chicas", 3.0, 7);
+const stickerHalloween = new Producto(6, "Sticker", "Stickers de Halloween", 2.75, 10);
+const stickerAnimales = new Producto(7, "Sticker", "Stickers de Animales", 3.5, 10);
 
 //Posters
-const posterNocheVerano = new Producto("Poster", "Poster de una Noche de Verano", 5.75, 6);
-const posterAmantesMariposa = new Producto("Poster", "Poster de Amantes Mariposa", 6.0, 8);
-const posterDeSanValentin = new Producto("Poster", "Poster de San Valentín espacial", 5.0, 10);
-const posterDeGatos = new Producto("Poster", "Poster de Gatos", 5.50, 7);
+const posterNocheVerano = new Producto(8, "Poster", "Poster de una Noche de Verano", 5.75, 6);
+const posterAmantesMariposa = new Producto(9, "Poster", "Poster de Amantes Mariposa", 6.0, 8);
+const posterDeSanValentin = new Producto(10, "Poster", "Poster de San Valentín espacial", 5.0, 10);
+const posterDeGatos = new Producto(11, "Poster", "Poster de Gatos", 5.50, 7);
 
 //Se crea el catálogo de búsqueda
 catalogoDeBusqueda.push(libretaChicasGamer, libretaSixFanarts, libretaLuluMartins, libretaChristineHug, stickerSirenas, stickerChicas, stickerHalloween, stickerAnimales, posterNocheVerano, posterAmantesMariposa, posterDeSanValentin, posterDeGatos);
@@ -270,6 +272,7 @@ function QUITARELEMENTOS() {
     }, carritoDeCompras);
     let menuOpcion = parseInt(prompt("Los Ítems cargados en el carrito de compras:\n" + conteo.join("\n") + "\nElija el Ítem que desea eliminar\n0- para volver al menú anterior."));
     if (menuOpcion > 0 && menuOpcion <= conteo.length) {
+        catalogoDeBusqueda[carritoDeCompras[(menuOpcion - 1)].id].stock = catalogoDeBusqueda[carritoDeCompras[(menuOpcion - 1)].id].stock + carritoDeCompras[(menuOpcion - 1)].cantidad;
         let borrado = carritoDeCompras.splice(menuOpcion - 1, 1);
         let itemEliminado = conteo.splice(menuOpcion - 1, 1);
         const listadoCarrito = carritoDeCompras.map((carritoDeCompras) => "-" + carritoDeCompras.cantidad + "x " + carritoDeCompras.nombre + "\n");
@@ -315,5 +318,3 @@ function CIERREDECOMPRA() {
 function EXITPROGRAM() {
     return alert("Está saliendo de la tienda, esperamos volver a verlo pronto.");
 }
-
-console.log(carritoDeCompras);
